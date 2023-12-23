2fd89bdf644d744f292bc5d642a1fdf0a334e77e
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-12-23 21:48:17 +0800

    add: command instruction

--- script.sh
+++ script.sh
@@ -4,3 +4,10 @@ sudo docker build -t yattt/from from
 #RUN
 sudo docker build -t yattt/run run 
 sudo docker build -t yattt/run run --progress=plain --no-cache  
+
+#COMMAND
+sudo docker build -t yattt/command command
+sudo docker image inspect yattt/command 
+sudo docker container create --name command yattt/command 
+sudo docker container start command 
+sudo docker container logs command  
