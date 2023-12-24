f791ac2e17ff510b4553144df4fe0d3663fc0e19
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-12-24 17:36:40 +0800

    add: label instruction

--- script.sh
+++ script.sh
@@ -11,3 +11,7 @@ sudo docker image inspect yattt/command
 sudo docker container create --name command yattt/command 
 sudo docker container start command 
 sudo docker container logs command  
+
+#LABEL
+sudo docker build -t yattt/label label
+sudo docker image inspect yattt/label  
